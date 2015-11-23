e4ac5e50c44d8d60a778f92dec09bf69f1ded0e4
1301657 <youtubegithub7>
2015-11-23 14:43:25 +0000

    alpha

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
  * Created by 1301657 on 23/11/2015.
  */
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response) {
