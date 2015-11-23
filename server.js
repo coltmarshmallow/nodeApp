48cdaf4753b9e36bd78ce29387d7c1e6e6cb1156
1301657 <youtubegithub7>
2015-11-23 14:45:55 +0000

    alpha

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ var port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
+    response.write('Connecting \n');
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+
+
     response.write("<!DOCTYPE 'html'>");
     response.write("<html>");
     response.write("<head>");
