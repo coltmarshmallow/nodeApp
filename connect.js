a19f61ca59970b32b5c8cc5990a3984b88dbcd82
1301657 <youtubegithub7>
2015-11-23 14:25:55 +0000

    alpha

---
+++ connect.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 1301657 on 23/11/2015.
+ */
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
