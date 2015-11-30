25bbb04efba69feaeee0c9b5ea63261559805f9f
1301657 <youtubegithub7>
2015-11-30 14:22:47 +0000

    twitter

--- twittertest.js
+++ twittertest.js
@@ -2,6 +2,9 @@
  * Created by 1301657 on 30/11/2015.
  */
 var Twitter = require('twitter');
+var http = require('http');
+var port = process.env.PORT || 1337;
+
 
 var client = new Twitter({
     consumer_key: 'Rsz60Vj2XNaxd5TeXevSp6civ',
@@ -10,7 +13,12 @@ var client = new Twitter({
     access_token_secret: 'bLhNRxW1FgCM2R4lhhFhSqEcPwhIAsGmvKv6kEn6dX9K1'
 });
 
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-client.get('search/tweets', {q: 'lolcat'}, function(error, tweets){
-    console.log(tweets);
-});
+
+    client.get('search/tweets', {q: 'lolcat'}, function (error, tweets) {
+        console.log(tweets);
+    });
+
+}).listen(port);
